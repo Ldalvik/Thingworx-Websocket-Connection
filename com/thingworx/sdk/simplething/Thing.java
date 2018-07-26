15a3740f05a2dbe546ed11d7cd26675ebf799a5b
Julian Osborne <subaruforge@gmail.com>
2018-07-26 10:32:38 -0400

    Update and rename SteamThing.java to Thing.java

---
+++ com/thingworx/sdk/simplething/Thing.java
@@ -0,0 +1,42 @@
+package com.thingworx.sdk.simplething;
+
+import com.thingworx.metadata.PropertyDefinition;
+import com.thingworx.communications.client.ConnectedThingClient;
+import com.thingworx.communications.client.things.VirtualThing;
+import java.util.List;
+
+public class Thing extends VirtualThing implements Runnable {
+
+    public Thing(String name, String description, String identifier, ConnectedThingClient client, List<PropertyDefinition> property) throws Exception {
+        super(name, description, identifier, client);
+        for(int i = 0; i < property.size(); i++) {
+            super.defineProperty(property.get(i));
+        }
+    }
+
+    public void synchronizeState() {
+        super.synchronizeState();
+        super.syncProperties();
+    }
+
+    @Override
+    public void processScanRequest() throws Exception {
+        super.processScanRequest();
+        this.scanDevice();
+    }
+
+    public void scanDevice() {
+        //super.updateSubscribedProperties(15000);
+        //super.updateSubscribedEvents(60000);
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(1000);
+            this.getClient().shutdown();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
